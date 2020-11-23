2157dbfb9364b9dcfcc1eeda0dbd77f69b246f66
Madina <Maulsharipova@gmail.com>
2020-11-23 13:26:07 +0600

    initial

---
+++ SearchApp/Controllers/Search.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SearchApp.Controllers
+{
+    public class Search : Controller
+    {
+      
+        public IActionResult GetResult()
+        {
+            return View();
+        }
+    }
+}
