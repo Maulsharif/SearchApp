3cca921f0705d2eefe6c4bc2ed4e5be2f222adfb
Madina <Maulsharipova@gmail.com>
2020-11-24 13:55:24 +0600

    удалила комментарий

--- SearchAppTest/SearchMockServiceTest.cs
+++ SearchAppTest/SearchMockServiceTest.cs
@@ -15,38 +15,28 @@ namespace SearchAppTest
         [Fact]
         public void ShouldThrowExceptionIfParameterIsEmpty()
         {
-            // arrange
             var service = new MoqSearchService();
-
-            // act and assert
             Assert.Throws<ArgumentException>(() => service.GetSearchResult(string.Empty));
         }
 
         [Fact]
         public void ShouldThrowExceptionIfParameterIsNull()
         {
-            // arrange
             var service = new MoqSearchService();
-
-            // act and assert
             Assert.Throws<ArgumentNullException>(() => service.GetSearchResult(null));
         }
 
         [Fact]
         public void ShouldReturnNotEmptyArray()
         {
-            // arrange
             var service = new MoqSearchService();
             var result = service.GetSearchResult("cat");
-
-            // act and assert
             Assert.NotNull(result);
         }
 
         [Fact]
         public void ShouldReturnEmptyArray()
         {
-            // arrange
             var service = new MoqSearchService();
             var result = service.GetSearchResult("bull");
             Assert.Equal(0, result.Count);
