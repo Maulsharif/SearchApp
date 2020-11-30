9497ba9f050a536d5fd3654f6375cb4b22d08605
Madina <Maulsharipova@gmail.com>
2020-11-30 16:35:38 +0600

    добавлен клиент для api

---
+++ SearchApp.UI/ClientApp/src/components/Search.tsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+
+
+
+
+const SearchInput=()=>{
+    return(
+            <div className="container">
+                <input  placeholder="Search..." onChange={}>
+
+                </input>
+            </div>
+        )
+
+
+
+}
+
+export default SearchInput;
